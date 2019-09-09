7f88e145f8701d17941437f04dde2bdb11a12c9b
Felix Wöstmann <f.wosti@gmail.com>
2019-09-09 11:47:57 +0200

    Changed programmatic approach
    Video will be prepared on start and after a video is finished.
    Videos can be sarted using the Enter key but only if a video is currently prepared and not playing.

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 //Constants
-const videodir="videos/";
-const extension=".mp4";
-const thumbnail="thumbnail.png";
-const highestVideoNumber=6;
-const numberOfRecentVideos=3;
+const videodir = "videos/";
+const extension = ".mp4";
+const thumbnail = "thumbnail.png";
+const highestVideoNumber = 6;
+const numberOfRecentVideos = 3;
 //Variables
-var lastVideos=new Array();
+var lastVideos = new Array();
 var video = document.getElementById("videoframe");
 var source = document.getElementById("videosource");
 
 function generateRandomNumberDifferentFromRecentVideos() {
-//Generates numbers from 0 - (highestVideoNumber-1) so we add 1
-var random=-1;
-do {
-random = Math.floor(Math.random() * highestVideoNumber)+1;
-} while (lastVideos.includes(random));
-addRecentVideoToArray(random);
-return random;
+  //Generates numbers from 0 - (highestVideoNumber-1) so we add 1
+  var random = -1;
+  do {
+    random = Math.floor(Math.random() * highestVideoNumber) + 1;
+  } while (lastVideos.includes(random));
+  addRecentVideoToArray(random);
+  return random;
 }
 
-function loadVideoAndPlay(filename) {
-var newvideo=videodir+filename+extension;
+function loadVideo(filename) {
+  var newvideo = videodir + filename + extension;
   video.pause();
   source.setAttribute('src', newvideo);
   video.load();
-  //videocontainer.setAttribute('poster', newposter); //Changes video poster image
-  video.play();
 }
 
 function addRecentVideoToArray(recentvideo) {
-  if(lastVideos.length>=numberOfRecentVideos) {
-      lastVideos.shift();
+  if (lastVideos.length >= numberOfRecentVideos) {
+    lastVideos.shift();
   }
   lastVideos.push(recentvideo);
 }
 
+video.onended = function() {
+  setUpVideo();
+};
 
-
+function setUpVideo() {
+  //Ist das aktuelle Video geendet, so suche ein neues aus und bereite es vor
+  var random = generateRandomNumberDifferentFromRecentVideos();
+  console.log(lastVideos.toString());
+  loadVideo(random);
+}
 document.addEventListener("keypress", function(event) {
-  //Video has ended won't work for the first pressed button
-  //so use source as flag if we are at the first video or later ones
-  var currentSource=source.getAttribute("src");
-if(currentSource==null||video.ended) {
-  if (event.keyCode == 13) { //Enter Key
-    var random=generateRandomNumberDifferentFromRecentVideos();
-    console.log(lastVideos.toString());
-      loadVideoAndPlay(random);
+  //Only take keypresses, when video is fully prepared
+  if (video.readyState == 4) {
+    if (event.keyCode == 13) { //Enter Key
+      video.play();
 
-}
-}
+    }
+  }
 
 })
 //TODO
