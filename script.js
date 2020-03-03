22a50ca35e5c1b9081b16facb94369f3a2091972
Felix Wöstmann <f.wosti@gmail.com>
2020-03-03 11:34:36 +0100

    Base Logic is implemented

--- script.js
+++ script.js
@@ -2,33 +2,44 @@
 const PICDIR = "pictures/";
 const EXTENSION = ".jpg";
 const HIGHESTPICNR = 11;
+const MAXTIMESDISPLAY=5;
 
 //Variables
 var pic_counter = new Array(HIGHESTPICNR);
 var img_container = document.getElementById("img_container");
 
+function init() {
+  initPicCounter();
+}
 
-function generateRandomNumberDifferentFromRecentVideos() {
-  //Generates numbers from 0 - (highestVideoNumber-1) so we add 1
+function initPicCounter() {
+  console.log(pic_counter)
+  var i;
+  for (i = 0; i < pic_counter.length; i++) {
+    pic_counter[i]=0;
+  }
+}
+
+function generateRandomNumberWithoutViolatingMaxTimesDisplay() {
+  //Generates numbers from 0 - (highestpicnr-1) so we add 1
   var random = -1;
   do {
-    random = Math.floor(Math.random() * HIGHESTVIDEONR) + 1;
-  } while (lastVideos.includes(random));
-  addRecentVideoToArray(random);
+    random = Math.floor(Math.random() * HIGHESTPICNR) + 1;
+  } while (pic_counter[random-1]>=MAXTIMESDISPLAY);
+  pic_counter[random-1] += 1;
   return random;
 }
 
-function loadVideo(filename) {
-  var newvideo = VIDEODIR + filename + EXTENSION;
-  video.pause();
-  source.setAttribute('src', newvideo);
-  video.load();
-}
 
 
 
 function chooseAndDisplayImage() {
-  img_container.setAttribute("src","pictures/1.jpg");
+  //Choose
+  var filenr=generateRandomNumberWithoutViolatingMaxTimesDisplay();
+  var new_img=PICDIR+filenr+EXTENSION;
+  console.log(pic_counter);
+  //SET
+  img_container.setAttribute("src",new_img);
 }
 
 document.addEventListener("keypress", function(event) {
